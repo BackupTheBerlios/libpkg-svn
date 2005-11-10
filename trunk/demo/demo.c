e318ab4db1b5c1f83296ac972e90c26eda41249b
zxombie <zxombie@94f56f15-1805-0410-9063-998f45cfa32b>
2005-11-10 23:34:25 +0000

    Update the demo to work with the current API

--- trunk/demo/demo.c
+++ trunk/demo/demo.c
@@ -2,6 +2,8 @@
 
 #include <errno.h>
 #include <pkg.h>
+#include <pkg_db.h>
+#include <pkg_repo.h>
 #include <stdlib.h>
 
 /* This is an internal function for the library not exported by pkg.h */
@@ -29,7 +31,7 @@ main (int argc, char *argv[])
 
 	argv++;
 	
-	if (pkg_dir_build("fakeroot/var/db/pkg") != PKG_OK) {
+	if (pkg_dir_build("fakeroot/var/db/pkg") != 0) {
 		fprintf(stderr, "ERROR: pkg_dir_build failed\n");
 		return EXIT_FAILURE;
 	}
@@ -39,12 +41,12 @@ main (int argc, char *argv[])
 	/* Open the package database and repo */
 	pkg_db = pkg_db_open_freebsd("fakeroot");
 	if (!pkg_db) {
-		fprintf(stderr, "ERROR: %s\n", pkg_error_string(&pkg_null));
+		fprintf(stderr, "ERROR: Couldn't open the package database\n");
 		return EXIT_FAILURE;
 	}
 	repo = pkg_repo_new_files();
 	if (!repo) {
-		fprintf(stderr, "ERROR: %s\n", pkg_error_string(&pkg_null));
+		fprintf(stderr, "ERROR: Couldn't open the package repo\n");
 		return EXIT_FAILURE;
 	}
 
@@ -55,25 +57,20 @@ main (int argc, char *argv[])
 
 		/* Check if the package is installed */
 		is_installed = pkg_db_is_installed(pkg_db, *argv);
-		if (is_installed == PKG_YES) {
+		if (is_installed == 0) {
 			fprintf(stderr, "Package %s is already installed\n",
 			    *argv);
 			continue;
-		} else if (is_installed == PKG_FAIL) {
-			fprintf(stderr, "Error checking if package %s is "
-			    "installed\n", *argv);
-			return EXIT_FAILURE;
-
 		/* Get the package from the repo */
 		} else if ((pkg = pkg_repo_get_pkg(repo, *argv)) == NULL) {
 			fprintf(stderr, "Package %s could not be found\n",
 			    *argv);
 		} else {
 			/* Install the package */
-			if (pkg_db_install_pkg(pkg_db, pkg) == PKG_FAIL) {
-				fprintf(stderr, "ERROR: %s\n",
-				    pkg_error_string((struct pkg_object *)
-				    pkg_db));
+			if (pkg_db_install_pkg(pkg_db, pkg) != 0) {
+				fprintf(stderr,
+				    "ERROR: Couldn't install package %s\n",
+				    *argv);
 			}
 			pkg_free(pkg);
 		}
