25b0a8d244ed62fe1acbc5c4ca48fb4d95b2ab62
zxombie <zxombie@94f56f15-1805-0410-9063-998f45cfa32b>
2006-03-20 02:22:52 +0000

    Document pkg.c in a way doxygen will be able to understand

--- trunk/src/pkg.c
+++ trunk/src/pkg.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005, Andrew Turner, All rights reserved.
+ * Copyright (C) 2005, 2006 Andrew Turner, All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,9 +33,25 @@
 #include "pkg.h"
 #include "pkg_private.h"
 
-/*
- * Creates a new package and associates callbacks that are
- * used by most types of packages
+/**
+ * @defgroup pkg_internal Internal package functions
+ * @ingroup pkg
+ * @brief Internal functions in the package module
+ *
+ * None of the callbacks for any given package format need to be specified.
+ * If they are NULL the accessor function will return an error.
+ * @{
+ */
+
+/**
+ * @brief Creates a new package and associates callbacks that are used
+ * by most types of packages.
+ * @param pkg_name The name of the package
+ * @param control_files A callback to be used by pkg_get_control_files()
+ * @param control_file A callback to be used by pkg_get_control_file()
+ * @param get_deps A callback to be used by pkg_get_dependencies()
+ * @param free_pkg A call back to be used by pkg_free()
+ * @return A new #pkg object, or NULL on error
  */
 struct pkg *
 pkg_new(const char *pkg_name,
@@ -80,9 +96,13 @@ pkg_new(const char *pkg_name,
 	return pkg;
 }
 
-/*
- * Internal function to add callbacks to retrieve data
+/**
+ * @brief Internal function to add callbacks to retrieve data
  * eg. the packages origin on FreeBSD
+ * @param pkg The package returned by pkg_new()
+ * @param get_version A callback to be used by pkg_get_version()
+ * @param get_origin A callback to be used by pkg_get_origin()
+ * @return 0 on success, -1 on error.
  */
 int
 pkg_add_callbacks_data(struct pkg *pkg,
@@ -97,8 +117,12 @@ pkg_add_callbacks_data(struct pkg *pkg,
 	return 0;
 }
 
-/*
- * Internal function to add callbacks that add data to the package
+/**
+ * @brief Internal function to add callbacks that add data to the package
+ * @param pkg The package returned by pkg_new()
+ * @param add_depend A callback to be used by pkg_get_dependencies()
+ * @param add_file A callback to be used by pkg_add_file()
+ * @return 0 on success, -1 on error.
  */
 int
 pkg_add_callbacks_empty(struct pkg *pkg, 
@@ -114,8 +138,12 @@ pkg_add_callbacks_empty(struct pkg *pkg,
 	return 0;
 }
 
-/*
- * Internal function to add callbacks that are used when a package is installed
+/**
+ * @brief Internal function to add callbacks that are used when a package is installed
+ * @param pkg The package returned by pkg_new()
+ * @param next_file A callback to be used by pkg_get_next_file()
+ * @return 0 on success, -1 on error.
+ * @return
  */
 int
 pkg_add_callbacks_install (struct pkg *pkg,
@@ -128,8 +156,27 @@ pkg_add_callbacks_install (struct pkg *pkg,
 	return 0;
 }
 
-/*
- * Creates an empty package with no callbacks
+/**
+ * @}
+ */
+
+/**
+ * @defgroup pkg Package manipulation Functions
+ * 
+ * These are the publicly availiable package manipulation functions.
+ *
+ * Most functions take a pointer to struct #pkg as the first argument.
+ * This is the package that is currently being worked on.
+ * @{
+ */
+
+/**
+ * @brief Creates an empty package with no callbacks
+ *
+ * This is the simplest package constructor.
+ * It is used to create a package with just a name associated with it.
+ * Only pkg_get_name() and pkg_compare() will work with the created package.
+ * @return An empty package
  */
 struct pkg*
 pkg_new_empty(const char *pkg_name)
@@ -137,19 +184,28 @@ pkg_new_empty(const char *pkg_name)
 	return pkg_new(pkg_name, NULL, NULL, NULL, NULL);
 }
 
-/*
- * A function to pass to *sort[_r] to sort alphabeticly by package name
+/**
+ * @brief A function to pass to *sort[_r] to sort alphabeticly by package name
+ * @param pkg_a the first package
+ * @param pkg_b the second package
+ *  
+ * This is used to lexigraphically compare two packages by their name.
+ * It is designed to be used with qsort() to sort a list of packages
+ * @return 0 > if pkg_a should be before pkg_b,
+ *         0 if both packages have the same name,
+ *         0 < if pkg_b should be before pkg_a.
  */
 int
 pkg_compare(const void *pkg_a, const void *pkg_b)
 {
-	/* XXX Makes WARNS <= 3 */
 	return strcmp((*(struct pkg * const *)pkg_a)->pkg_name,
 	    (*(struct pkg * const *)pkg_b)->pkg_name);
 }
 
-/*
- * Gets the control files from a given package
+/**
+ * @brief Gets the control files from a given package
+ * 
+ * @return A null-terminated array of pkg_file's contining the packages control files
  */
 struct pkg_file **
 pkg_get_control_files(struct pkg *pkg)
@@ -165,8 +221,10 @@ pkg_get_control_files(struct pkg *pkg)
 	return pkg->pkg_get_control_files(pkg);
 }
 
-/*
- * Gets a given control file from a package
+/**
+ * @brief Gets a given control file from a package
+ * @param pkg_name The name of the file to return
+ * @return The control file with the name pkg_name
  */
 struct pkg_file *
 pkg_get_control_file(struct pkg *pkg, const char *pkg_name)
@@ -180,8 +238,11 @@ pkg_get_control_file(struct pkg *pkg, const char *pkg_name)
 	return NULL;
 }
 
-/*
- * Gets all the dependencies for a given package
+/**
+ * @brief Gets all the dependencies for a given package
+ *
+ * This retrieves an array of packages that the named package depends on.
+ * @return A NULL terminated array of packages or NULL;
  */
 struct pkg **
 pkg_get_dependencies(struct pkg *pkg)
@@ -194,8 +255,10 @@ pkg_get_dependencies(struct pkg *pkg)
 	return NULL;
 }
 
-/*
- * Gets the name of a package
+/**
+ * @brief Gets the name of a package
+ *
+ * @return a sting containing the package name. Do not free this.
  */
 const char *
 pkg_get_name(struct pkg *pkg)
@@ -205,9 +268,12 @@ pkg_get_name(struct pkg *pkg)
 	return pkg->pkg_name;
 }
 
-/*
- * Gets the next file in a package, used for installation
- * to iterate over all files to be installed in a package
+/**
+ * @brief Gets the next file in a package.
+ *
+ * Ths is used during the instillation of a package to iterate over
+ * all files to be installed in a package
+ * @return the next non-control file in the package or NULL when done.
  */
 struct pkg_file *
 pkg_get_next_file(struct pkg *pkg)
@@ -223,8 +289,12 @@ pkg_get_next_file(struct pkg *pkg)
 	return pkg->pkg_get_next_file(pkg);
 }
 
-/*
- * Gets a packages origin if it has one
+/**
+ * @brief Gets a packages origin
+ *
+ * This is used to get the package origin from packages that record it.
+ * Not all package formats have an origin.
+ * @return The packages origin or NULL if it dosn't have one.
  */
 const char *
 pkg_get_origin(struct pkg *pkg)
@@ -238,8 +308,12 @@ pkg_get_origin(struct pkg *pkg)
 	return NULL;
 }
 
-/*
- * Return a string containg the package format version
+/**
+ * @brief Get the package format version
+ *
+ * This retrieves a free form string containing the package format's version.
+ * It is intended to be shown to the user rather than be processed.
+ * @return A string containing the package format version. Do not free.
  */
 const char *
 pkg_get_version(struct pkg *pkg)
@@ -253,8 +327,9 @@ pkg_get_version(struct pkg *pkg)
 	return NULL;
 }
 
-/*
- * Adds a dependency to a given package
+/**
+ * @brief Adds a dependency to a given package
+ * @return 0 on success, -1 on error.
  */
 int
 pkg_add_dependency(struct pkg *pkg, struct pkg *depend)
@@ -268,8 +343,9 @@ pkg_add_dependency(struct pkg *pkg, struct pkg *depend)
 	return -1;
 }
 
-/*
- * Adds a file to a given package
+/**
+ * @brief Adds a file to a given package
+ * @return 0 on success, -1 on error.
  */
 int
 pkg_add_file(struct pkg *pkg, struct pkg_file *file)
@@ -283,8 +359,12 @@ pkg_add_file(struct pkg *pkg, struct pkg_file *file)
 	return -1;
 }
 
-/*
- * Frees a NULL terminated array of packages, eg. from pkg_get_dependencies
+/**
+ * @brief Frees a NULL terminated array of packages
+ *
+ * This is to be used to free the arrays generated by
+ * pkg_get_dependencies()
+ * @return 0 on success, -1 on error.
  */
 int
 pkg_list_free(struct pkg **pkgs)
@@ -302,8 +382,9 @@ pkg_list_free(struct pkg **pkgs)
 	return 0;
 }
 
-/*
- * Frees a given package
+/**
+ * @brief Frees a given package
+ * @return 0 on success, -1 on error.
  */
 int
 pkg_free(struct pkg *pkg)
@@ -322,3 +403,7 @@ pkg_free(struct pkg *pkg)
 
 	return 0;
 }
+
+/**
+ * @}
+ */
