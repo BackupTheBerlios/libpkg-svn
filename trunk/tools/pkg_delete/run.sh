6541da0cc7fa96553685634fc7f1be3519eb40c3
zxombie <zxombie@94f56f15-1805-0410-9063-998f45cfa32b>
2006-10-26 23:28:44 +0000

    Add the start of functional tests

---
+++ trunk/tools/pkg_delete/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PACKAGE=$1
+shift
+TESTNO=$1
+
+if [ ${TESTNO} -eq 1 ] ; then
+	pkg_add ./${PACKAGE}.tbz
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+	pkg_delete ${PACKAGE}
+	exit $?
+elif [ ${TESTNO} -eq 2 ] ; then
+fi
---
+++ trunk/tools/pkg_delete/test.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+. `dirname $0`/../test.sh
+
+# Rins the tests for either the libpkg or cvs version of pkg_delete
+do_test() {
+	RUN=$1
+
+	i=1
+	while [ $i -le 1 ] ; do
+		chroot ${BASE_DIR}/base /run.sh ${PACKAGE} ${i} > ${RUN}.stdout.${i} 2> ${RUN}.stderr.${i}
+		# Get the mtree file to use to compare the filesystems
+		mtree -c -p ${BASE_DIR}/base | grep -v "^\#[[:space:]]*date:" | sed "s/time=[^ ]*//" > ${RUN}.mtree.${i}
+		# Create a tarball of the important dir's to compare later
+		rm ${BASE_DIR}/${RUN}-${i}.tar
+		tar -cf ${BASE_DIR}/${RUN}-${i}.tar ${BASE_DIR}/base/var/db/pkg ${BASE_DIR}/base/usr/local
+		# Cleanup the package directories
+		rm -fr ${BASE_DIR}/base/var/db/pkg ${BASE_DIR}/base/usr/local
+
+		i=$((i+1))
+	done
+}
+
+# Runs the test
+run_test() {
+
+	# Get the reference data from the FreeBSD cvs pkg_delete
+	build_chroot
+	do_test cvs
+
+	# Get the test data from out pkg_delete
+	build_chroot
+	build_tool
+	cp pkg_delete ${BASE_DIR}/base/usr/sbin/pkg_delete
+	do_test libpkg
+}
+
+CWD=`pwd`
+cd `dirname $0`
+
+run_test
+
+cd $CWD
