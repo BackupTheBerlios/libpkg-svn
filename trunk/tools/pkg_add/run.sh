4a128046b956512ae91884007f33846082ab0795
zxombie <zxombie@94f56f15-1805-0410-9063-998f45cfa32b>
2007-01-27 02:16:08 +0000

    Add more pkg_add tests Allow each pkg_add test to be run separately

--- trunk/tools/pkg_add/run.sh
+++ trunk/tools/pkg_add/run.sh
@@ -24,4 +24,29 @@ elif [ ${TESTNO} -eq 4 ] ; then
 	if [ $? -ne 0 ] ; then
 		exit 1
 	fi
+elif [ ${TESTNO} -eq 5 ] ; then
+	pkg_add -I ./${PACKAGE}.tbz
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+elif [ ${TESTNO} -eq 6 ] ; then
+	pkg_add -R ./${PACKAGE}.tbz
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+elif [ ${TESTNO} -eq 7 ] ; then
+	pkg_add -f ./${PACKAGE}.tbz
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+elif [ ${TESTNO} -eq 8 ] ; then
+	pkg_add -p /usr/pkg ./${PACKAGE}.tbz
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+elif [ ${TESTNO} -eq 9 ] ; then
+	pkg_add -P /usr/pkg ./${PACKAGE}.tbz
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
 fi
--- trunk/tools/pkg_add/test.sh
+++ trunk/tools/pkg_add/test.sh
@@ -2,26 +2,36 @@
 
 . `dirname $0`/../test.sh
 
-# Rins the tests for either the libpkg or cvs version of pkg_add
+TEST_NO=$1
+shift
+
 do_test() {
+	i=$1
+	RUN=$2
+	LIBPKG=$3
+
+	build_chroot
+	if [ "X$LIBPKG" != "X" ]; then
+		cp pkg_add ${BASE_DIR}/base/usr/sbin/pkg_add
+	fi
+	chroot ${BASE_DIR}/base /run.sh ${PACKAGE} ${i} > ${RUN}.stdout.${i} 2> ${RUN}.stderr.${i}
+	# Get the mtree file to use to compare the filesystems
+	mtree -c -p ${BASE_DIR}/base | grep -v "^\#[[:space:]]*date:" | sed "s/time=[^ ]*//" | grep -v "^[ ]*pkg_add[^\.]" > ${RUN}.mtree.${i}
+	# Create a tarball of the important dir's to compare later
+	rm ${BASE_DIR}/${RUN}-${i}.tar
+	tar -cf ${BASE_DIR}/${RUN}-${i}.tar ${BASE_DIR}/base/var/db/pkg ${BASE_DIR}/base/usr/local ${BASE_DIR}/base/usr/pkg
+	# Cleanup the package directories
+	rm -fr ${BASE_DIR}/base/var/db/pkg ${BASE_DIR}/base/usr/local ${BASE_DIR}/base/usr/pkg
+}
+
+# Rins the tests for either the libpkg or cvs version of pkg_add
+do_tests() {
 	RUN=$1
 	LIBPKG=$2
 
 	i=1
-	while [ $i -le 4 ] ; do
-		build_chroot
-		if [ "X$LIBPKG" != "X" ]; then
-			cp pkg_add ${BASE_DIR}/base/usr/sbin/pkg_add
-		fi
-		chroot ${BASE_DIR}/base /run.sh ${PACKAGE} ${i} > ${RUN}.stdout.${i} 2> ${RUN}.stderr.${i}
-		# Get the mtree file to use to compare the filesystems
-		mtree -c -p ${BASE_DIR}/base | grep -v "^\#[[:space:]]*date:" | sed "s/time=[^ ]*//" | grep -v "^[ ]*pkg_add[^\.]" > ${RUN}.mtree.${i}
-		# Create a tarball of the important dir's to compare later
-		rm ${BASE_DIR}/${RUN}-${i}.tar
-		tar -cf ${BASE_DIR}/${RUN}-${i}.tar ${BASE_DIR}/base/var/db/pkg ${BASE_DIR}/base/usr/local
-		# Cleanup the package directories
-		rm -fr ${BASE_DIR}/base/var/db/pkg ${BASE_DIR}/base/usr/local
-
+	while [ $i -le 9 ] ; do
+		do_test $i $RUN $LIBPKG
 		i=$((i+1))
 	done
 }
@@ -29,12 +39,17 @@ do_test() {
 # Runs the test
 run_test() {
 
-	# Get the reference data from the FreeBSD cvs pkg_add
-	do_test cvs
-
-	# Get the test data from out pkg_add
 	build_tool
-	do_test libpkg true
+
+	if [ "X${TEST_NO}" != "X" ] ; then
+		do_test $TEST_NO cvs
+		do_test $TEST_NO libpkg true
+	else
+		# Get the reference data from the FreeBSD cvs pkg_add
+		do_tests cvs
+		# Get the test data from out pkg_add
+		do_tests libpkg true
+	fi
 }
 
 CWD=`pwd`
