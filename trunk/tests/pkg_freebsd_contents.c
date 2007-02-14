432fe642732f5aa52db58334ce89f4f2fa4f79ec
zxombie <zxombie@94f56f15-1805-0410-9063-998f45cfa32b>
2007-02-14 06:53:45 +0000

    Add the start of tests for FreeBSD +CONTENTS file processing Rename the targets to run the test and the test program's name

--- trunk/tests/pkg_freebsd_contents.c
+++ trunk/tests/pkg_freebsd_contents.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006, 2007 Andrew Turner, All rights reserved.
+ * Copyright (C) 2007, Andrew Turner, All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,37 +28,55 @@
  */
 
 #include "test.h"
-#include <stdlib.h>
 
-int
-setup_testdir()
+#include <pkg.h>
+#include <pkg_freebsd.h>
+
+#include <string.h>
+
+START_TEST(pkg_freebsd_contents_empty_test)
 {
-	system("rm -fr testdir");
-	return system("mkdir testdir");
+	struct pkg_freebsd_contents *contents;
+
+	contents = pkg_freebsd_contents_new("", 0);
+	fail_unless(contents == NULL, NULL);
+	pkg_freebsd_contents_free(contents);
+}
+END_TEST
+
+/* Test if a minimal config file will work */
+START_TEST(pkg_freebsd_contents_good_basic_test)
+{
+	struct pkg_freebsd_contents *contents;
+	const char *pkg_data = "@comment PKG_FORMAT_REVISION:1.1\n"
+	    "@name package_name-1.0\n"
+	    "@comment ORIGIN:package/origin\n"
+	    "@cwd /usr/local\n";
+
+	contents = pkg_freebsd_contents_new(pkg_data, strlen(pkg_data));
+	fail_unless(contents != NULL, NULL);
+	pkg_freebsd_contents_free(contents);
+}
+END_TEST
+
+Suite *
+pkg_freebsd_contents_suite()
+{
+	Suite *s;
+	TCase *tc;
+
+	s = suite_create("pkg_freebsd_contents");
+
+	tc = tcase_create("empty");
+	tcase_add_test(tc, pkg_freebsd_contents_empty_test);
+	suite_add_tcase(s, tc);
+
+
+	tc = tcase_create("good");
+	tcase_add_test(tc, pkg_freebsd_contents_good_basic_test);
+	suite_add_tcase(s, tc);
+
+	return s;
 }
 
-int
-cleanup_testdir()
-{
-	if (system("rmdir testdir") == 0)
-		return 0;
 
-	system("rm -fr testdir");
-	return 1;
-}
-
-int
-main(int argc __unused, char *argv[] __unused)
-{
-	int fail_count;
-	SRunner *sr;
-
-	sr = srunner_create(pkgfile_suite());
-	srunner_add_suite(sr, pkg_freebsd_contents_suite());
-
-	srunner_run_all(sr, CK_NORMAL);
-	fail_count = srunner_ntests_failed(sr);
-	srunner_free(sr);
-
-	return (fail_count == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
-}
--- trunk/tests/test.h
+++ trunk/tests/test.h
@@ -6,4 +6,5 @@ int cleanup_testdir(void);
 #define CLEANUP_TESTDIR() fail_unless(cleanup_testdir() == 0, "Couldn't cleanup the test dir")
 
 Suite *pkgfile_suite(void);
+Suite *pkg_freebsd_contents_suite(void);
 
