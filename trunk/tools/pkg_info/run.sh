c1440c9d67f578dcffef9b4e03640165740475a0
zxombie <zxombie@94f56f15-1805-0410-9063-998f45cfa32b>
2007-03-12 06:03:30 +0000

    Add more pkg_info tests

--- trunk/tools/pkg_info/run.sh
+++ trunk/tools/pkg_info/run.sh
@@ -40,4 +40,28 @@ elif [ ${TESTNO} -eq 6 ] ; then
 	if [ $? -ne 0 ] ; then
 		exit 1
 	fi
+elif [ ${TESTNO} -eq 7 ] ; then
+	pkg_add ./${PACKAGE}.tbz
+	pkg_info -r ${PACKAGE}
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+elif [ ${TESTNO} -eq 8 ] ; then
+	pkg_add -p /usr/pkg ./${PACKAGE}.tbz
+	pkg_info -r ${PACKAGE}
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+elif [ ${TESTNO} -eq 9 ] ; then
+	pkg_add ./${PACKAGE}.tbz
+	pkg_info -p ${PACKAGE}
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
+elif [ ${TESTNO} -eq 10 ] ; then
+	pkg_add -p /usr/pkg ./${PACKAGE}.tbz
+	pkg_info -p ${PACKAGE}
+	if [ $? -ne 0 ] ; then
+		exit 1
+	fi
 fi
