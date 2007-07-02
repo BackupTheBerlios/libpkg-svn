22ef5a72bc2c8e63775c259728f27a6eee7250b1
zxombie <zxombie@94f56f15-1805-0410-9063-998f45cfa32b>
2007-07-02 07:09:28 +0000

    Update the license to the FreeBSD version

--- trunk/src/pkg_manifest.c
+++ trunk/src/pkg_manifest.c
@@ -1,29 +1,27 @@
 /*
- * Copyright (C) 2007, Andrew Turner, All rights reserved.
+ * Copyright (C) 2007, Andrew Turner
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer
- *    in this position and unchanged.
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. The name(s) of the author(s) may not be used to endorse or promote
- *    products derived from this software without specific prior written
- *    permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  *
  */
 
@@ -126,8 +124,7 @@ pkg_manifest_free(struct pkg_manifest *manifest)
 		free(manifest->item_list);
 
 	for (pos = 0; pos < pkgm_max; pos++) {
-		if (manifest->attrs[pos] != NULL &&
-		    manifest->attrs[pos] != (char *)0x1)
+		if (manifest->attrs[pos] != NULL)
 			free(manifest->attrs[pos]);
 	}
 
@@ -533,8 +530,7 @@ pkg_manifest_item_free(struct pkg_manifest_item *item)
 	if (item->attrs != NULL) {
 		unsigned int pos;
 		for (pos = 0; pos < pmia_max; pos++) {
-			if (item->attrs[pos] != NULL &&
-			    item->attrs[pos] != (char *)0x1)
+			if (item->attrs[pos] != NULL)
 				free(item->attrs[pos]);
 		}
 		free(item->attrs);
@@ -598,12 +594,12 @@ pkg_manifest_item_set_attr(struct pkg_manifest_item *item,
 		    sizeof(pkg_manifest_item_attr));
 	}
 
-	if (item->attrs[attr] != NULL && item->attrs[attr] != (char *)0x1) {
+	if (item->attrs[attr] != NULL) {
 		free(item->attrs[attr]);
 	}
 
 	if (data == NULL) {
-		item->attrs[attr] = (char *)0x1;
+		item->attrs[attr] = NULL;
 	} else {
 		item->attrs[attr] = strdup(data);
 		if (item->attrs[attr] == NULL)
